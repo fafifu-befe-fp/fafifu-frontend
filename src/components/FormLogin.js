32252dfd7322d68596e958209a4408a7eec7fb7f
Aditya Ramday <87653433+ramd24@users.noreply.github.com>
2022-06-15 11:23:10 +0700

    login template no fixing

---
+++ src/components/FormLogin.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const FormLogin = () => {
+
+  return (
+    <div className='container-fluid'>
+        <div className='row'>
+            <div className='col-sm-6'>
+                <img src='img/login.png' />
+                <img src='img/secondhand.svg' />
+            </div>
+            <div className='col-sm-6 '>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default FormLogin
