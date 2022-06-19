9be045138d7cd1cfbf493baab40865a5602b0ab8
MANSHUR\cungm <muhammadnashir67@gmail.com>
2022-06-19 08:46:21 +0700

    Homepage layout (categories, product)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
-import Info from './Pages/Profile/Info';
-import Login from './Pages/Login/Login';
-import Register from './Pages/Register/Register'
+import Navbar from './components/Navbar/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './Pages/Home/Home';
+import List from './Pages/List/List';
+
+// import Info from './Pages/Profile/Info';
+// import Login from './Pages/Login/Login';
+// import Register from './Pages/Register/Register'
 
 function App() {
   return (
-    <div className="App">
-      <Register />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/list" element={<List />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
