a636939984e539adf284d0110a4297e4fe00568f
MANSHUR\cungm <muhammadnashir67@gmail.com>
2022-06-24 19:46:57 +0700

    product, slider - update layout

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,11 +5,11 @@ const Card = () => {
   return (
     <div className='container'>
       <div className="card p-2">
-        <img className="card-img-top" src="https://i.pinimg.com/564x/91/9d/ad/919dad52c0f041b6442c8b452bd6111a.jpg" alt="Card image" />
+        <img className="card-img-top" src="../../img/sepatu-static.jpg" alt="Card image" />
         <div className="card-body">
-          <h5 className="card-title">I Might Regret This</h5>
-          <small className="card-text text-muted">Buku</small>
-          <p className='mt-2'>Rp50.000</p>
+          <h5 className="card-title">Limited Edition Series</h5>
+          <small className="card-text text-muted">Sepatu</small>
+          <p className='mt-2'>Rp250.000</p>
         </div>
       </div>
     </div>
